c209847fa17984cb96bb9b5a9d646cb1435d1114
Chris Jewell <chrisjohnjewell@gmail.com>
2019-02-26 21:30:29 +1100

    Remove unused imports in MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import {
   Container,
   Header,
