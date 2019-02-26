f34661e1e69cb516ef0bd29cefa0f142a30fcffc
Chris Jewell <chrisjohnjewell@gmail.com>
2019-02-26 20:41:23 +1100

    Change constants to point to 2019 data

--- constants.js
+++ constants.js
@@ -1,22 +1,27 @@
 module.exports = {
   COLOR: {
-    YFFRED: '#C7563D',
-    YFFGREEN: '#9A9B52',
-    YFFTEAL: '#4C8579',
-    YFFBROWN: '#352B1F',
-    YFFORANGE: '#E4752E',
-    YFFLIGHTGREY: '#D8D8D8'
+    YFFRED: "#C7563D",
+    YFFGREEN: "#9A9B52",
+    YFFTEAL: "#4C8579",
+    YFFBROWN: "#352B1F",
+    YFFORANGE: "#E4752E",
+    YFFLIGHTGREY: "#D8D8D8"
   },
   DATES: {
-    FRI: '24 Mar',
-    SAT: '25 Mar',
-    SUN: '26 Mar'
+    FRI: "22 Mar",
+    SAT: "23 Mar",
+    SUN: "24 Mar"
   },
   ENDPOINTS: {
-    ALLPERFORMANCES: 'https://raw.githubusercontent.com/RustComet/YFFJSON/master/all_performances_remote.json',
-    ARTISTS: 'https://raw.githubusercontent.com/RustComet/YFFJSON/master/artists_remote.json',
-    FRIPERFORMANCES: 'https://raw.githubusercontent.com/RustComet/YFFJSON/master/fri_performances_remote.json',
-    SATPERFORMANCES: 'https://raw.githubusercontent.com/RustComet/YFFJSON/master/sat_performances_remote.json',
-    SUNPERFORMANCES: 'https://raw.githubusercontent.com/RustComet/YFFJSON/master/sun_performances_remote.json'
+    ALLPERFORMANCES:
+      "https://raw.githubusercontent.com/RustComet/YFFJSON/master/staging/all_performances_remote.json",
+    ARTISTS:
+      "https://raw.githubusercontent.com/RustComet/YFFJSON/master/staging/artists_remote.json",
+    FRIPERFORMANCES:
+      "https://raw.githubusercontent.com/RustComet/YFFJSON/master/staging/fri_performances_remote.json",
+    SATPERFORMANCES:
+      "https://raw.githubusercontent.com/RustComet/YFFJSON/master/staging/sat_performances_remote.json",
+    SUNPERFORMANCES:
+      "https://raw.githubusercontent.com/RustComet/YFFJSON/master/staging/sun_performances_remote.json"
   }
 };
