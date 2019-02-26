81bb74d5f58456baaa9c7547d642709f7515ad0c
Chris Jewell <chrisjohnjewell@gmail.com>
2019-02-26 21:27:37 +1100

    Make MapScreen a functional component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -13,38 +13,33 @@ import {
 import PhotoView from "react-native-photo-view";
 import GLOBAL from "../constants";
 
-class MapScreen extends Component {
-  render() {
-    return (
-      <Container>
-        <Header style={{ backgroundColor: GLOBAL.COLOR.YFFORANGE }}>
-          <Left>
-            <Button transparent>
-              <Icon
-                name="menu"
-                onPress={() => this.props.navigation.openDrawer()}
-              />
-            </Button>
-          </Left>
-          <Body>
-            <Title style={styles.title}>Event Map</Title>
-          </Body>
-          <Right />
-        </Header>
-        <View style={styles.container}>
-          <PhotoView
-            source={require("../assets/mapImage.png")}
-            minimumZoomScale={1}
-            maximumZoomScale={3}
-            androidScaleType="center"
-            onLoad={() => console.log("Image loaded!")}
-            style={{ height: "100%" }}
-          />
-        </View>
-      </Container>
-    );
-  }
-}
+MapScreen = ({ navigation }) => {
+  return (
+    <Container>
+      <Header style={{ backgroundColor: GLOBAL.COLOR.YFFORANGE }}>
+        <Left>
+          <Button transparent>
+            <Icon name="menu" onPress={() => navigation.openDrawer()} />
+          </Button>
+        </Left>
+        <Body>
+          <Title style={styles.title}>Event Map</Title>
+        </Body>
+        <Right />
+      </Header>
+      <View style={styles.container}>
+        <PhotoView
+          source={require("../assets/mapImage.png")}
+          minimumZoomScale={1}
+          maximumZoomScale={3}
+          androidScaleType="center"
+          onLoad={() => console.log("Image loaded!")}
+          style={{ height: "100%" }}
+        />
+      </View>
+    </Container>
+  );
+};
 
 const styles = StyleSheet.create({
   title: {
