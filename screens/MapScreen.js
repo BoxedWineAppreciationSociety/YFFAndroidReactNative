e7d774a16a080ff4a5b52427e3a5f6d5c4109bfe
Chris Jewell <chrisjohnjewell@gmail.com>
2019-02-26 21:25:17 +1100

    Format MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import {
-  View,
-  Text,
-  SafeAreaView,
-  StyleSheet
-} from 'react-native';
-import { Container, Header, Left, Right, Icon, Title, Button, Body } from 'native-base';
-import PhotoView from 'react-native-photo-view';
-import GLOBAL from '../constants';
+  Container,
+  Header,
+  Left,
+  Right,
+  Icon,
+  Title,
+  Button,
+  Body
+} from "native-base";
+import PhotoView from "react-native-photo-view";
+import GLOBAL from "../constants";
 
 class MapScreen extends Component {
-  render(){
+  render() {
     return (
       <Container>
-        <Header style={{backgroundColor: GLOBAL.COLOR.YFFORANGE}}>
+        <Header style={{ backgroundColor: GLOBAL.COLOR.YFFORANGE }}>
           <Left>
             <Button transparent>
-              <Icon name='menu' onPress={() => this.props.navigation.openDrawer()} />
+              <Icon
+                name="menu"
+                onPress={() => this.props.navigation.openDrawer()}
+              />
             </Button>
           </Left>
           <Body>
             <Title style={styles.title}>Event Map</Title>
           </Body>
-          <Right>
-          </Right>
+          <Right />
         </Header>
         <View style={styles.container}>
-        <PhotoView
-          source={require('../assets/mapImage.png')}
-          minimumZoomScale={1}
-          maximumZoomScale={3}
-          androidScaleType="center"
-          onLoad={() => console.log("Image loaded!")}
-          style={{height: '100%'}} />
+          <PhotoView
+            source={require("../assets/mapImage.png")}
+            minimumZoomScale={1}
+            maximumZoomScale={3}
+            androidScaleType="center"
+            onLoad={() => console.log("Image loaded!")}
+            style={{ height: "100%" }}
+          />
         </View>
       </Container>
     );
@@ -41,13 +48,13 @@ class MapScreen extends Component {
 
 const styles = StyleSheet.create({
   title: {
-    fontFamily: 'SourceSansPro-Regular',
-    fontSize: 24,
+    fontFamily: "SourceSansPro-Regular",
+    fontSize: 24
   },
   container: {
     flex: 1,
     zIndex: -1
   }
-})
+});
 
-export default MapScreen
+export default MapScreen;
