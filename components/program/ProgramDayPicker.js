baf6c988c73296e53f1099d5546c359944540dd8
Chris Jewell <chrisjohnjewell@gmail.com>
2019-02-26 21:29:56 +1100

    Remove unused imports in ProgramDayPicker

--- components/program/ProgramDayPicker.js
+++ components/program/ProgramDayPicker.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { StyleSheet, View, ImageBackground, Text } from "react-native";
-import { Left, Title, Right, Button, Container } from "native-base";
+import { Button } from "native-base";
 
 import GLOBAL from "../../constants";
 
